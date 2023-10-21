e543dfdc8482cfd03bdef885ccb08e27e82f7f8d
Alan Fernandez <alanf718@gmail.com>
2023-10-21 10:04:24 -0400

    linting fix

--- src/math/3d/line.ts
+++ src/math/3d/line.ts
@@ -2,23 +2,23 @@ import { Pos3, Vector3 } from '../';
 import { add, direction, scale, magnitude } from '../3d';
 
 export interface Line3 {
-  a: Pos3;
-  b: Pos3;
-  len: number;
-  d: Vector3;
-  unit_d: Vector3;
+	a: Pos3;
+	b: Pos3;
+	len: number;
+	d: Vector3;
+	unit_d: Vector3;
 }
 
 export const createLine = (a: Pos3, b: Pos3): Line3 => {
-  const d = direction(a, b);
-  const len = magnitude(d);
-  return {
-    a,
-    b,
-    d,
-    len,
-    unit_d: scale(d, 1 / len),
-  };
+	const d = direction(a, b);
+	const len = magnitude(d);
+	return {
+		a,
+		b,
+		d,
+		len,
+		unit_d: scale(d, 1 / len),
+	};
 };
 
 // export const lineIntersection = (line1: Line3, line2: Line3): Pos | undefined => {
@@ -39,15 +39,15 @@ export const createLine = (a: Pos3, b: Pos3): Line3 => {
 
 // maxed out at [0, 1]
 export const interpolate = (line: Line3, t: number): Pos3 => {
-  const val = Math.min(Math.max(0, t), 1);
-  return add(line.a, scale(line.d, val));
+	const val = Math.min(Math.max(0, t), 1);
+	return add(line.a, scale(line.d, val));
 };
 
 // maxed out at [0, len]
 export const interpolated = (line: Line3, distance: number): [Pos3, number] => {
-  const remainder = Math.max(0, distance - line.len);
-  const d = Math.min(Math.max(0, distance), line.len);
-  return [add(line.a, scale(line.unit_d, d)), remainder];
+	const remainder = Math.max(0, distance - line.len);
+	const d = Math.min(Math.max(0, distance), line.len);
+	return [add(line.a, scale(line.unit_d, d)), remainder];
 };
 
 /**
@@ -55,16 +55,16 @@ export const interpolated = (line: Line3, distance: number): [Pos3, number] => {
  * will return undefined, otherwise if clamp is passed in, it will clamp to the min/max pos of the line
  */
 export const getPosWithRespectToY = (
-  line: Line3,
-  y: number,
-  clamp?: true
+	line: Line3,
+	y: number,
+	clamp?: true
 ): Pos3 | undefined => {
-  const dy = y - line.a.y;
-  const t = dy / line.d.dy;
-  if ((t >= 0 && t <= 1) || clamp) {
-    return interpolate(line, t);
-  }
-  return undefined;
+	const dy = y - line.a.y;
+	const t = dy / line.d.dy;
+	if ((t >= 0 && t <= 1) || clamp) {
+		return interpolate(line, t);
+	}
+	return undefined;
 };
 
 /**
@@ -72,16 +72,16 @@ export const getPosWithRespectToY = (
  * will return undefined, otherwise if clamp is passed in, it will clamp to the min/max pos of the line
  */
 export const getPosWithRespectToX = (
-  line: Line3,
-  x: number,
-  clamp?: true
+	line: Line3,
+	x: number,
+	clamp?: true
 ): Pos3 | undefined => {
-  const dx = x - line.a.x;
-  const t = dx / line.d.dx;
-  if ((t >= 0 && t <= 1) || clamp) {
-    return interpolate(line, t);
-  }
-  return undefined;
+	const dx = x - line.a.x;
+	const t = dx / line.d.dx;
+	if ((t >= 0 && t <= 1) || clamp) {
+		return interpolate(line, t);
+	}
+	return undefined;
 };
 
 /**
@@ -89,16 +89,16 @@ export const getPosWithRespectToX = (
  * will return undefined, otherwise if clamp is passed in, it will clamp to the min/max pos of the line
  */
 export const getPosWithRespectToZ = (
-  line: Line3,
-  z: number,
-  clamp?: true
+	line: Line3,
+	z: number,
+	clamp?: true
 ): Pos3 | undefined => {
-  const dz = z - line.a.z;
-  const t = dz / line.d.dz;
-  if ((t >= 0 && t <= 1) || clamp) {
-    return interpolate(line, t);
-  }
-  return undefined;
+	const dz = z - line.a.z;
+	const t = dz / line.d.dz;
+	if ((t >= 0 && t <= 1) || clamp) {
+		return interpolate(line, t);
+	}
+	return undefined;
 };
 
 /**
@@ -110,12 +110,12 @@ export const getPosWithRespectToZ = (
  * @returns {number} distance of point from teh start of the supplied line
  */
 export const getDfromPosition = (line: Line3, pos: Pos3): number => {
-  const dVector = direction(line.a, pos);
-  return magnitude(dVector);
+	const dVector = direction(line.a, pos);
+	return magnitude(dVector);
 };
 
 export const translate = (line: Line3, { dx, dy, dz }: Vector3): Line3 => ({
-  ...line,
-  a: { x: line.a.x + dx, y: line.a.y + dy, z: line.a.z + dz },
-  b: { x: line.b.x + dx, y: line.b.y + dy, z: line.b.z + dz },
+	...line,
+	a: { x: line.a.x + dx, y: line.a.y + dy, z: line.a.z + dz },
+	b: { x: line.b.x + dx, y: line.b.y + dy, z: line.b.z + dz },
 });
